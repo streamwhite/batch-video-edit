001f8715d70b7958e6e12728bf13946ff65698f1
streamwhite <streamwhite@hotmail.com>
2024-08-27 21:33:37 +0800

    fix win setup script

--- public/shell-scripts/running it/windows_powershell_run.ps1
+++ public/shell-scripts/running it/windows_powershell_run.ps1
@@ -1,19 +1,10 @@
+# go to the project directory and run this script
+
 # run this script in PowerShell
 
-
-# Step 1: Install dependencies
-Write-Output "Installing dependencies..."
-npm install
-
-# Step 2: Build the project
-Write-Output "Building the project..."
-npm run build
-
-# Step 3: Start the project
 Write-Output "Starting the project..."
 Start-Process "npm" "run start"
 
-# Step 4: Open the project in the default web browser
 Write-Output "Opening http://localhost:3000/ in the default web browser..."
 Start-Process "http://localhost:3000/"
 
--- public/shell-scripts/set-up-running-environment/windows_powershell_setup_and_run.ps1
+++ public/shell-scripts/set-up-running-environment/windows_powershell_setup_and_run.ps1
@@ -2,6 +2,11 @@
 
 # run this script in PowerShell with administrator privileges
 
+# install Chocolatey and verify the installation
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+choco --version
+
 # Function to check if a package is installed and install it if not
 function Install-ChocoPackageIfNotInstalled {
     param (
@@ -29,3 +34,21 @@ Install-ChocoPackageIfNotInstalled -packageName "ffmpeg-full"
 # Clone the project
 Write-Output "Cloning the project..."
 git clone https://github.com/streamwhite/batch-video-edit.git
+
+# Change the working directory to the project directory
+Write-Output "Changing the working directory to the project directory..."
+cd "batch-video-edit"
+
+Write-Output "Installing dependencies..."
+npm install
+
+Write-Output "Building the project..."
+npm run build
+
+Write-Output "Starting the project..."
+Start-Process "npm" "run start"
+
+Write-Output "Opening http://localhost:3000/ in the default web browser..."
+Start-Process "http://localhost:3000/"
+
+Write-Output "Project setup and started successfully."
