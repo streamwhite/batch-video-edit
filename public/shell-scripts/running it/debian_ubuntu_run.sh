5c9860153469607f54865d159933cb08a7d01c0c
streamwhite <streamwhite@hotmail.com>
2024-08-27 21:28:03 +0800

    run from one command

--- public/shell-scripts/running it/debian_ubuntu_run.sh
+++ public/shell-scripts/running it/debian_ubuntu_run.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-
-
-
-
-# Step 1: Install dependencies
-echo "Installing dependencies..."
-npm install
-
-# Step 2: Build the project
-echo "Building the project..."
-npm run build
-
-# Step 3: Start the project
 echo "Starting the project..."
 npm run start &
 
