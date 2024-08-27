f0df49c3e67702a29abc1ed5cfd378cc7ac19a68
streamwhite <streamwhite@hotmail.com>
2024-08-27 23:21:25 +0800

    ignore files

--- public/shell-scripts/running it/macOS_run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Starting the project..."
-npm run start &
-
-echo "Opening http://localhost:3000/ in the default web browser..."
-open http://localhost:3000/
-
-echo "Project setup and started successfully."
